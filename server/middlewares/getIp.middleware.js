91d8f3207c606e974a73d1b58799d1adb30c8288
Angelo García <161162882+ANHELL0S@users.noreply.github.com>
2024-07-28 09:37:35 -0500

    Delete server directory

--- server/middlewares/getIp.middleware.js
+++
@@ -1,8 +0,0 @@
-// Code by OpenAI
-function getIpAddress(req, res, next) {
-	const forwarded = req.headers['x-forwarded-for']
-	req.userIp = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
-	next()
-}
-
-export { getIpAddress }
